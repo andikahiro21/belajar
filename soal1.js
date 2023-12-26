2db2d4ac80cc2571a6f5cc476871401cf91a7c57
andikahiro21 <andika.pratama@phincon.com>
2023-12-27 00:55:23 +0700

    first commit

---
+++ soal1.js
@@ -0,0 +1,7 @@
+const nilai_matematika = 90;
+const nilai_fisika = 60;
+const nilai_biologi = 50;
+
+//buatkan function untuk cek nilai
+//jika average nilai yaitu (nilai1+nilai2+nilai3 / 3 ) lebih dari 75 cetak lulus
+// jika kurang dari 75 cetak tidak lulus
---
+++ soal2.js
@@ -0,0 +1,5 @@
+const panjang = 5;
+const lebar = 7;
+
+//buatlah rumus persegi panjang dengan function
+//jawabannya harus 35
